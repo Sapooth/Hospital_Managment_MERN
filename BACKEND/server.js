7c0afb23cfe4cad164b0574917b3a36101b8805f
Sapooth Sanjaya <sapoothskoop11@gmail.com>
2023-01-04 22:54:05 +0530

    nw commmit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors  = require("cors");
 const dotenv   = require("dotenv");
-const app = express();  
+const app = express();
+require("dotenv").config();  
 
 const PORT = process.env.PORT || 8070;
 
@@ -13,15 +14,15 @@ app.use(bodyParser.json());
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL, {
-  useCreateIndex: true,
+ 
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+ 
 });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
-    console.log("Mongodb connection success!");
+    console.log("Mongodb Connection success!");
 })
 
 app.listen(PORT,() => {

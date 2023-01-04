e2dd6ec8a8f85d453ffcfc3dc3fe323104def7e5
Sapooth <sapoothskoop11@gmail.com>
2023-01-05 01:23:39 +0530

    Created Add

--- BACKEND/routes/patients.js
+++ BACKEND/routes/patients.js
@@ -1,2 +1,34 @@
 const router = require("express").Router();
-let Student
+let Patient = require("../models/Patient");
+
+router.route("/add").post((req,res)=>{
+
+    const first_name = req.body.first_name;
+    const lirst_name = req.body.last_name;
+    const age = Number(req.body.age);
+    const address = req.body.address;
+    const phone_no = Number(req.body.phone_no);
+    const disease = req.body.disease;
+    const gender = req.body.gender;
+
+    const newPatient = new Patient({
+
+        first_name,
+        lirst_name,
+        age,
+        address,
+        phone_no,
+        disease,
+        gender
+    }) 
+
+    newPatient.save().then(()=>{
+        res.json("Student Added")
+    }).catch(()=>{
+        console.log(err);
+    })
+
+
+})
+
+module.exports = router;

245f5d610f4aad206e385c5ad8113b4dfabc553b
Sapooth Sanjaya <sapoothskoop11@gmail.com>
2023-01-04 20:21:50 +0530

    first commit

---
+++ BACKEND/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors  = require("cors");
+const dotenv   = require("dotenv");
+const app = express();  
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Mongodb connection success!");
+})
+
+app.listen(PORT,() => {
+    console.log('Server is up and running on port number: ${PORT}')
+})

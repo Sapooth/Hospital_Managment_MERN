9b0bf129ba9510e49a8bcabde291be9ca0bf2086
Sapooth <sapoothskoop11@gmail.com>
2023-01-05 17:44:06 +0530

    Created Delete function

--- BACKEND/routes/patients.js
+++ BACKEND/routes/patients.js
@@ -61,8 +61,29 @@ router.route("/update/:id").put(async (req, res) => {
         console.log(err);
         res.status(500).send({status: "Error with updating data", error: err.message});
     })
-
 })
 
+router.route("/delete/:id").delete(async (req,res) => {
+    let userId = req.params.id;
+
+    await Patient.findByIdAndDelete(userId)
+    .then(()=>{
+        res.status(200).send({status: "User Deleted"})
+    }).catch((err)=>{
+        console.log(err.message);
+        resstatus(500).send({status: "Error with delete user", error:err.message});
+    })
+})
+
+router.route("/get/:id").get(async(req,res)=>{
+    let userId = req.params.id;
+    const user = await Patient.findById(userId)
+    .then(()=>{
+        res.status(200).send({status:"User fetched",user: user})
+    }).catch(()=>{
+        console.log(err.message);
+        res.status(500).send({status:"Error with get user", error: err.message});
+    })
+})
 
 module.exports = router;

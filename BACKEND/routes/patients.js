55b91f86743cc328cec0d7bc113b91c591e030f5
Sapooth <sapoothskoop11@gmail.com>
2023-01-05 15:49:22 +0530

    Created Update function

--- BACKEND/routes/patients.js
+++ BACKEND/routes/patients.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const { response } = require("express");
 let Patient = require("../models/Patient");
 
 router.route("/add").post((req,res)=>{
 
     const first_name = req.body.first_name;
-    const lirst_name = req.body.last_name;
+    const last_name = req.body.last_name;
     const age = Number(req.body.age);
     const address = req.body.address;
     const phone_no = Number(req.body.phone_no);
@@ -14,7 +15,7 @@ router.route("/add").post((req,res)=>{
     const newPatient = new Patient({
 
         first_name,
-        lirst_name,
+        last_name,
         age,
         address,
         phone_no,
@@ -36,6 +37,29 @@ router.route("/").get((req,res)=>{
         res.json(patients)
     }).catch((err)=>{
         console.log
+    })
+
+})
+
+router.route("/update/:id").put(async (req, res) => {
+    let userId = req.params.id;
+    const {first_name,last_name,age,address,phone_no,disease,gender} = req.body;
+
+    const updatePatient = {
+        first_name,
+        last_name,
+        age,
+        address,
+        phone_no,
+        disease,
+        gender
+    }
+    const update = await Patient.findByIdAndUpdate(userId,updatePatient)
+    .then(()=>{
+        res.status(200).send({status:"User update", user: update})
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status: "Error with updating data", error: err.message});
     })
 
 })

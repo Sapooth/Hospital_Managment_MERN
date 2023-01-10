6f1396b278fac8100b8e6ecbdf148578dd91a76f
sachindinuja <121999384+sachindinuja@users.noreply.github.com>
2023-01-10 08:37:59 +0530

    FRONTEND BASIC

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import CounterClass from "./components/CounterClass";
+import CounterFunction from "./components/CounterFuntion";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO WORLD</h1>
+      <CounterClass/>
+      <CounterFunction/>
+    </div>
+  );
+}
+
+export default App;

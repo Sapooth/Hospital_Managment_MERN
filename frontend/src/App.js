697508f43beef482ee95e55569ebec5627e227e0
Sapooth <sapoothskoop11@gmail.com>
2023-01-18 11:14:13 +0530

    Created Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
-import CounterClass from './components/CounterClass';
-import CounterFunction from './components/CounterFunction';
+import Header from './components/Header';
+
 
 function App() {
+
   return (
    <div>
-
     
+    <Header/>
    </div>
   );
 }
